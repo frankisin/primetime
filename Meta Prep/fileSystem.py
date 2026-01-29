843aea5f8f322ee644bad82aee1f627da7611c1c
frankisin <velazquezfra96@gmail.com>
2026-01-29 10:07:28 -0500

    finished update cap get file size and delete file

--- Meta Prep/fileSystem.py
+++ Meta Prep/fileSystem.py
@@ -48,7 +48,67 @@ def solution(queries):
                     out.append("true")
                 else:
                     out.append("false")
-                    
+        elif op == "FIND_FILES":
+            matches = []
+            prefix = q[1]
+            suffix = q[2]
+            
+            for path,(size,owner) in files.items():
+                pathname = str(path)
+                if(pathname.startswith(prefix) and pathname.endswith(suffix)):
+                    matches.append((size,path))
+                
+                #sort by descending size then path...
+            matches.sort(key=lambda x: (-x[0],x[1]))
+            out.append(",".join(f"{path}({size})" for size,path in matches))
+        elif op == "UPDATE_CAPACITY":
+            user = q[1]
+            new_capacity = int(q[2])
+            
+            if not user_exists(user):
+                out.append("false")
+            else:
+                if user != "admin":
+                    used_space = used[user]
+                    if used_space > new_capacity:
+                        out.append("false")
+                    else:
+                        users[user] = new_capacity
+                        out.append("true")
+                else:
+                    out.append("true")
+        elif op == "GET_FILE_SIZE":
+            filepath = q[1]
+            
+            if filepath not in files:
+                out.append("false")
+            else:
+                out.append(str(files[filepath][0]))
+        elif op == "DELETE_FILE":
+            filepath = q[1]
+            if filepath not in files:
+                out.append("false")
+            else:
+                (filesize,owner) = files[filepath]
+                
+                if owner != "admin":
+                    used[owner] -= filesize
+                
+                del files[filepath]
+                out.append(str(filesize))
+               
+                
+            
+                
+            
+            
+            
+            
+                
+
+                
+                
+                
 
             
 
