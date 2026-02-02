4576bca24edf7649f4421b332f5c7b074a498b28
frankisin <velazquezfra96@gmail.com>
2026-02-02 15:08:18 -0500

    added logic for permissions

---
+++ Meta Prep/oa_review/permissions.py
@@ -0,0 +1,11 @@
+def solution(queries):
+    users = {} # userId (str) : roles set(roleName)
+    roles = {} #roleName (str) : permissions set(permissions)
+    
+    for q in queries:
+        op = q[0]
+        
+        if op == "":
+            pass
+    
+    
